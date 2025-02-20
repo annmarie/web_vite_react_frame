192fbd5d0a5cc237854864b6ae3a641c88105d06
Annmarie Conway <annmarie.daylife@gmail.com>
2025-02-19 19:08:30 -0500

    removed unneeded code

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,6 @@
 import { render } from "@testing-library/react";
 import App from "./App";
 
-global.TextEncoder = require('util').TextEncoder;
-
 describe("App", () => {
   it('should matche the snapshot', () => {
     const { asFragment } = render(<App />);
