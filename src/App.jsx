37b82f5f3d70b5a8a1ec129d12e7fc41b92276d1
Annmarie Conway <annmarie.daylife@gmail.com>
2025-02-20 08:37:11 -0500

    added backgammon to hello page

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ const Hello = () => {
       <li><Link to="/catcarousel">CatCarousel</Link></li>
       <li><Link to="/pegsolitaire">PegSolitaire</Link></li>
       <li><Link to="/diceroll">DiceRoll</Link></li>
+      <li><Link to="/backgammon">Backgammon (WIP)</Link></li>
     </ul>
   )
 }
